554373ce4e052463fe7a86be32a9e92ab50bf5d8
watan7 <willkietan95@gmail.com>
2017-12-29 23:56:57 +1100

    Add files via upload

--- Road Camera.py
+++ Road Camera.py
@@ -1,6 +1,8 @@
+#Loading Dependencies
 from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
+from future import standard_library
+standard_library.install_aliases()
+
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -8,23 +10,26 @@ from urllib.error import HTTPError
 
 import json
 import os
+import requests 
 
 from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
-app = Flask(__name__)
 
+#Receiving HTTP Request
+app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
+    
+    camera_title = camera_title(req)
+    
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = processRequest(req, camera_title)
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -32,41 +37,37 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
+#returns title name of camera
+def camera_title(req):
     if req.get("result").get("action") != "Road_Camera_Call":
         return {}
-    baseurl = "https://api.transport.nsw.gov.au/v1/live/cameras"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    result = urlopen(baseurl).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
 
-data1 = data['features'][0:]
-titles = []
-for index in data1:
-    titles.append(index['properties']['title'])
-    print(index['properties']['title'])
-index = titles.index(camera_title)    
-
-
-def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
     camera_title = parameters.get("camera_title")
     if camera_title is None:
-        return None
-
+        return {}
     return camera_title
 
+#Loads data from Live Cameras API - Transport NSW and Makes webhook result
+def processRequest(req, camera_title):
+    baseurl = "https://api.transport.nsw.gov.au/v1/live/cameras"
+    response = requests.get(baseurl, headers={'Authorization': 'apikey pYIdffDyBMqdtActEnbbBcajSC3gEBdTkAtx'})
+    data = response.json()
+    res = makeWebhookResult(data, camera_title)
+    return res
 
-def makeWebhookResult(data):
+#Makes result sent to DialogueFlow
+def makeWebhookResult(data, camera_title):
     
+    data1 = data['features'][0:]
+    titles = []
+    for index in data1:
+        titles.append(index['properties']['title'])
+    index = titles.index(camera_title)   
+
     features = data.get('features')
-    if query is None:
+    if features is None:
         return {}
 
     camera = features[index]
@@ -83,19 +84,18 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Here is a photo of the traffic in" + camera_title + href
+    speech = "Here is a photo of the traffic in "
 
     print("Response:")
-    print(speech)
+    print(speech, camera_title, href)
 
     return {
-        "speech": speech,
+        "speech": speech + camera_title + href, 
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
         "source": "Road_Camera_Robot"
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
