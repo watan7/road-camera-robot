bb053c7fde64588d090d268b8484b88968837e78
watan7 <willkietan95@gmail.com>
2018-01-04 19:02:39 +1100

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,10 @@ def webhook():
     elif req.get("originalRequest").get("source") == "facebook":
     	coordinates = req.get("originalRequest").get("data").get("data")
         res = coordinates
-        
+    
+    else:
+    	res = "nothing was returned"
+    
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
