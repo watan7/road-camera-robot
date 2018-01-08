9154f7973893d0734e9c016a6640026058e8ac6e
watan7 <willkietan95@gmail.com>
2018-01-09 03:09:22 +1100

    Update app.py

--- app.py
+++ app.py
@@ -150,8 +150,8 @@ def makeWebhookResult_stopFinder(data):
         
     for i in range(len(names)):
         speech.append("{}: {} is {}m away - https://www.google.com/maps/search/?api=1&query={},{}\
-                               
-                      ".format(i + 1, names[i], distance[i], lat[i], long[i]))
+                               \
+                               ".format(i + 1, names[i], distance[i], lat[i], long[i]))
     
     speech = ''.join(speech)
         
