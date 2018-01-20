35d073d8dc107caa68f97056a4b358c0d5eca900
watan7 <willkietan95@gmail.com>
2018-01-20 22:28:46 +1100

    Update app.py

--- app.py
+++ app.py
@@ -315,8 +315,7 @@ def makeWebhookResult_trainTrip(train_trips):
 
 
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
